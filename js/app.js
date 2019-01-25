034004751e91ab441aaf0eba8c9c43cd2a030fae
Harsimran <harsimransinghbarki@gmail.com>
2019-01-25 20:32:49 +0530

    Added Media Queries For Navbar

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let menu = document.getElementById('menu');
+let button = document.getElementById('button');
+
+function add() {
+    menu.style.display = "block";
+}
+
+function back() {
+    menu.style.display = "none";
+}
